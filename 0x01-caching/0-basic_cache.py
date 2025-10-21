928823d8643b5fdcfe9909707393991406822c1e
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-10-21 10:24:57 +0200

    task 0.1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,10 +6,12 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """basic caching system"""
     def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value for the key key"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """return the value in self.cache_data linked to key"""
         if not key or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
